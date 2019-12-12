67a68d311932463025f2cc007699d47a24845ee1
Richard Hagen <richard.hagen@gmail.com>
2019-12-12 10:26:35 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import sketch from './sketches/mic'
+import P5Wrapper from 'react-p5-wrapper';
 
 function App() {
+  const [[sources, selected], setSources] = React.useState([[],""])
+
+  const sourcesUpdated = (sources) => {
+    setSources([sources, selected])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Microphone (Feedback) Test
       </header>
+      <P5Wrapper sketch={sketch} selectedSource={selected} sourcesUpdated={sourcesUpdated}/>
+
+      <select value={selected} onChange={e => setSources([sources, e.target.value])}>
+        {sources.map((s,i) => <option key={s.deviceId} value={i}>{s.label}</option>)}
+      </select>
     </div>
   );
 }
 
 export default App;
+
+/*
+ MediaDeviceInfo
+​​
+deviceId: "TIJdALKjdnt1ClJ+95/U4wV4thx/QzlIFuyeTYTMJ3s="
+​​
+groupId: "AJHvC/cX/73IHfGR5k9D6IgKyU09vX5eBbfaskoVqRs="
+​​
+kind: "audioinput"
+​​
+label: "Meteorite condenser microphone Analog Stereo"
+*/
---
+++ src/sketches/mic.js
@@ -0,0 +1,102 @@
+import p5 from 'p5'
+import 'p5/lib/addons/p5.sound'
+
+export default function sketch(p){
+    let canvas;
+    let mic;
+    let micHistory = []
+    let signalHistory = []
+    let sourcesUpdated = false;
+    let selectedSource = 0;
+    let signal
+    let signalAmp = 0.0
+    let signalFreq = 1000
+    let time = 0
+    let frequencies = [100, 250, 500, 750, 1000,2000,4000,8000]
+
+
+    p.setup = () => {
+      canvas = p.createCanvas(1080, 540);
+    
+      // Create an Audio input
+      mic = new p5.AudioIn();
+    
+      // start the Audio Input.
+      // By default, it does not .connect() (to the computer speakers)
+      mic.start();
+      mic.getSources().then(sources => {
+        if (sourcesUpdated)
+          sourcesUpdated(sources)
+      })
+      mic.amp(1.0)
+
+
+      signal = new p5.Oscillator('sine');
+      signal.amp(signalAmp); // set amplitude
+      signal.freq(signalFreq); // set frequency
+      signal.start(); // start oscillating
+    }
+
+    p.myCustomRedrawAccordingToNewPropsHandler = function(newProps){
+      sourcesUpdated = newProps.sourcesUpdated
+      if (newProps.selectedSource && newProps.selectedSource !== selectedSource){
+        selectedSource = newProps.selectedSource
+        mic.setSource(selectedSource)
+      }
+    }
+
+    let triggerSignals = () => {
+      time++
+      if (time % 50 !== 0)
+        return
+
+      signalAmp = 1.0
+      signal.amp(signalAmp); // set amplitude
+      console.log(signalFreq)
+      setTimeout(() => {
+        signalAmp = 0.0
+        signal.amp(signalAmp); // set amplitude
+
+        signalFreq = frequencies[frequencies.findIndex(s => s === signalFreq) + 1 % frequencies.length]
+        signal.freq(signalFreq)
+      }, 100)
+    }
+
+    p.draw = () => {
+      triggerSignals()
+      p.background(0);
+    
+      // Get the overall volume (between 0 and 1.0)
+      let micVol = mic.getLevel();
+      micHistory.push(micVol)
+      signalHistory.push(signalAmp)
+
+      // p.fill(127);
+      p.noFill();
+    
+
+      // Draw the signal history
+      p.stroke([50, 0,0]);
+      p.beginShape()
+      for (let i = 0; i < signalHistory.length; i++) {
+        const y = p.map(signalHistory[i], 0, 1, canvas.height, 0);
+        p.vertex(i, y);
+      }
+      p.endShape()
+
+      // Draw the mic history
+      p.stroke([0, 0,255]);
+      p.beginShape()
+      for (let i = 0; i < micHistory.length; i++) {
+        const y = p.map(micHistory[i], 0, 1, canvas.height, 0);
+        p.vertex(i, y);
+      }
+      p.endShape()
+
+
+      if (micHistory.length > canvas.width)
+        micHistory.shift()
+      if (signalHistory.length > canvas.width)
+      signalHistory.shift()
+    }
+}
